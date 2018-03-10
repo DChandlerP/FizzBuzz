3e0825d7f281f31b237beb54ba56822403850ac8
DChandlerP <chandler.prince@gmail.com>
2018-03-09 18:30:56 -0600

    Changed method names and collapsed everything.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,17 +1,16 @@
+//Used methods to make it easier to read.
+
 for (let number = 1; number < 101; number++){
-
-    if(divisibleBy3(number) && divisibleBy5(number)){
-        console.log("FizzBuzz");
-    }
-    else if (divisibleBy3(number)){console.log("Fizz")}
-    else if (divisibleBy5(number)){console.log("Buzz")}
-    else {console.log(number)}
+    if(isDivisibleBy3(number) && isDivisibleBy5(number)) console.log("FizzBuzz");
+    else if (isDivisibleBy3(number)) console.log("Fizz")
+    else if (isDivisibleBy5(number)) console.log("Buzz")
+    else console.log(number)
 }
 
-function divisibleBy3(number) {
-    return number %3 === 0;
+function isDivisibleBy3(number) {
+    return number % 3 === 0;
 }
 
-function divisibleBy5(number) {
-    return number % 5 ===0;
+function isDivisibleBy5(number) {
+    return number % 5 === 0;
 }
